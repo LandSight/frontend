bbcfcacded97ec5943bbdf9a242dd845e8fb20d9
ShavrinAleksei <apshavrinwork@gmail.com>
2026-03-22 17:37:36 +0300

    feature(navigation): switch to reatom

--- src/app/layouts/Sidebar/NavigationMenu/NavigationMenu.tsx
+++ src/app/layouts/Sidebar/NavigationMenu/NavigationMenu.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { wrap } from '@reatom/core';
 
+import { routesConfig } from '#/app/routes/routesConfig';
 import { cn } from '#/shared/lib/bem';
-
-import { getMenuItems } from './helpers';
 
 import './NavigationMenu.scss';
 
 const cnNavMenu = cn('NavigationMenu');
 
-export const NavigationMenu: React.FC = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const menuItems = getMenuItems();
+export const NavigationMenu = () => {
+  const menuItems = Object.values(routesConfig).filter((r) => r.showInMenu);
 
   return (
     <List className={cnNavMenu()}>
-      {menuItems.map((item) => {
-        const isActive = location.pathname === item.path;
+      {menuItems.map(({ text, icon, route, exact }) => {
+        const isActive = exact ? route.exact() : route();
+
         return (
-          <ListItem key={item.text} disablePadding className={cnNavMenu('ItemWrapper')}>
+          <ListItem key={text} disablePadding className={cnNavMenu('ItemWrapper')}>
             <ListItemButton
-              selected={isActive}
-              onClick={() => navigate(item.path)}
-              className={cnNavMenu('Item', { selected: isActive })}
+              selected={!!isActive}
+              onClick={wrap(() => route.go())}
+              className={cnNavMenu('Item', { selected: !!isActive })}
             >
-              <ListItemIcon className={cnNavMenu('ItemIcon')}>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.text} className={cnNavMenu('ItemText')} />
+              <ListItemIcon className={cnNavMenu('ItemIcon')}>{icon}</ListItemIcon>
+              <ListItemText primary={text} className={cnNavMenu('ItemText')} />
             </ListItemButton>
           </ListItem>
         );
