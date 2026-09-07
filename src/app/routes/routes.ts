93dc4bc353c4571b4f18ecb9829fda353533465d
ShavrinAleksei <apshavrinwork@gmail.com>
2026-09-07 20:39:59 +0300

    feat(routes): add auth routes

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,5 +1,7 @@
 import { reatomRoute } from '@reatom/core';
 
+export const loginRoute = reatomRoute('login');
+export const registerRoute = reatomRoute('register');
 export const mapRoute = reatomRoute('map');
 export const analysesRoute = reatomRoute('analyses');
 export const reportRoute = reatomRoute('reports/:id');
