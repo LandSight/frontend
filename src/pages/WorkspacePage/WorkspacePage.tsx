73538ce85adf68e5e2ee305e52d4bd5ba82da8a8
ShavrinAleksei <apshavrinwork@gmail.com>
2026-09-23 00:26:05 +0300

    feat(pages): remove old pages and add new workspace page

---
+++ src/pages/WorkspacePage/WorkspacePage.tsx
@@ -0,0 +1,65 @@
+import { useEffect } from 'react';
+import { Box } from '@mui/material';
+import { wrap } from '@reatom/core';
+import { reatomComponent } from '@reatom/react';
+
+import { AnalysisStartDialog } from '#/features/analyses/components/AnalysisStartDialog';
+import { fetchAnalyses } from '#/features/analyses/models';
+import { hasActiveAnalysesAtom } from '#/features/analyses/models/analyses';
+import { fetchInfrastructureCategories } from '#/features/infrastructure/models';
+import { MapView } from '#/features/map/components/MapView';
+import { clearDrawingState } from '#/features/map/models';
+import { CreateParcelDialog } from '#/features/parcels/components/CreateParcelDialog';
+import {
+  fetchParcels,
+  isCreateParcelDialogOpenAtom,
+  newParcelNameAtom,
+} from '#/features/parcels/models';
+import { WorkspaceSidebar } from '#/features/workspace/components/WorkspaceSidebar';
+import { useWorkspaceUrlSync } from '#/features/workspace/hooks';
+
+const POLL_INTERVAL_MS = 3000;
+
+export const WorkspacePage = reatomComponent(() => {
+  useWorkspaceUrlSync();
+
+  const hasActiveAnalyses = hasActiveAnalysesAtom();
+
+  useEffect(() => {
+    fetchParcels();
+    fetchAnalyses();
+    fetchInfrastructureCategories();
+  }, []);
+
+  useEffect(() => {
+    if (!hasActiveAnalyses) {
+      return;
+    }
+    const intervalId = setInterval(() => {
+      fetchAnalyses();
+    }, POLL_INTERVAL_MS);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [hasActiveAnalyses]);
+
+  const isCreateParcelDialogOpen = isCreateParcelDialogOpenAtom();
+
+  const handleCloseCreateParcelDialog = () => {
+    wrap(isCreateParcelDialogOpenAtom.close());
+    wrap(clearDrawingState());
+    wrap(newParcelNameAtom.reset());
+  };
+
+  return (
+    <Box sx={{ display: 'flex', height: '100%', overflow: 'hidden' }}>
+      <Box sx={{ flex: 1, position: 'relative', minWidth: 0 }}>
+        <MapView />
+      </Box>
+      <WorkspaceSidebar />
+
+      <AnalysisStartDialog />
+      <CreateParcelDialog open={isCreateParcelDialogOpen} onClose={handleCloseCreateParcelDialog} />
+    </Box>
+  );
+}, 'WorkspacePage');
