3a815287bd2e3a30ab008f1062eeef7e975f11b9
ShavrinAleksei <apshavrinwork@gmail.com>
2026-09-23 10:14:04 +0300

    refactor(parcel): use reatom style

--- src/features/parcels/components/ParcelListContainer/ParcelList/ParcelList.tsx
+++ src/features/parcels/components/ParcelListContainer/ParcelList/ParcelList.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import type { MouseEvent } from 'react';
 import { Delete } from '@mui/icons-material';
 import { IconButton, List, ListItem, ListItemText } from '@mui/material';
+import { reatomComponent } from '@reatom/react';
 
 import { cn } from '#/shared/lib/bem';
 
@@ -10,42 +11,40 @@ import './ParcelList.scss';
 
 const cnParcelList = cn('ParcelList');
 
-export const ParcelList: React.FC<ParcelListProps> = ({
-  parcels,
-  selectedParcelId = null,
-  onClick,
-  onDelete,
-}) => {
-  const handleClick = (id: string) => onClick?.(id);
-  const handleDelete = (id: string, e: React.MouseEvent) => {
-    e.stopPropagation();
-    onDelete?.(id);
-  };
+export const ParcelList = reatomComponent<ParcelListProps>(
+  ({ parcels, selectedParcelId = null, onClick, onDelete }) => {
+    const handleClick = (id: string) => onClick?.(id);
+    const handleDelete = (id: string, e: MouseEvent) => {
+      e.stopPropagation();
+      onDelete?.(id);
+    };
 
-  return (
-    <List dense className={cnParcelList()}>
-      {parcels.map((parcel) => (
-        <ListItem
-          key={parcel.id}
-          className={cnParcelList('Item', { selected: selectedParcelId === parcel.id })}
-          onClick={() => handleClick(parcel.id)}
-          secondaryAction={
-            <>
-              {onDelete && (
-                <IconButton
-                  size="small"
-                  onClick={(e) => handleDelete(parcel.id, e)}
-                  title="Удалить"
-                >
-                  <Delete className={cnParcelList('Icon')} fontSize="small" />
-                </IconButton>
-              )}
-            </>
-          }
-        >
-          <ListItemText primary={parcel.name} />
-        </ListItem>
-      ))}
-    </List>
-  );
-};
+    return (
+      <List dense className={cnParcelList()}>
+        {parcels.map((parcel) => (
+          <ListItem
+            key={parcel.id}
+            className={cnParcelList('Item', { selected: selectedParcelId === parcel.id })}
+            onClick={() => handleClick(parcel.id)}
+            secondaryAction={
+              <>
+                {onDelete && (
+                  <IconButton
+                    size="small"
+                    onClick={(e) => handleDelete(parcel.id, e)}
+                    title="Удалить"
+                  >
+                    <Delete className={cnParcelList('Icon')} fontSize="small" />
+                  </IconButton>
+                )}
+              </>
+            }
+          >
+            <ListItemText primary={parcel.name} />
+          </ListItem>
+        ))}
+      </List>
+    );
+  },
+  'ParcelList'
+);
